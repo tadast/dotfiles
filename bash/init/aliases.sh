9b7f4626a28f39b587f0e31649e56f5687ebfde6
Tadas Tamosauskas <tadastamo@gmail.com>
2020-08-25 11:36:25 +0100

    Alias for opening current PWD in file browser

--- bash/init/aliases.sh
+++ bash/init/aliases.sh
@@ -48,3 +48,4 @@ alias open=xdg-open
 alias edit=subl
 alias zshrc='edit ~/.zshrc'
 alias screencast='sleep 3 && ffmpeg -y -f x11grab -r 25 -s 1920x1080 -i :0.0 -vcodec libx265 -crf 28 ~/Downloads/screencast.mp4'
+alias browse='nautilus . > /dev/null 2>&1 &'
